785a668edd3654da4828569a38fa5246446efda3
KyungHyun Jovinus Lee <lkh256@gmail.com>
2021-03-13 15:07:09 +0900

    Test

--- main.py
+++ main.py
@@ -16,3 +16,7 @@ print(f"Ones Tensor: \n {x_ones} \n")
 
 x_rand = torch.rand_like(x_data, dtype=torch.float) # overrides the datatype of x_data
 print(f"Random Tensor: \n {x_rand} \n")
+
+print("Moo Yaa Ho!")
+
+print("I love VSCODE")
